fb541ac8e6ff2147ae3072cc7657794a36fd0d0e
Robson Andrade <robsom.s.a@gmail.com>
2017-03-29 23:22:26 -0300

    Adding index.html

---
+++ scss/CompileSass.cmd
@@ -0,0 +1,5 @@
+D:
+cd ../../../../../../
+cd "Program Files (x86)\Ruby23-x64\bin"
+sass "D:\Program Files (x86)\EasyPHP-DevServer-14.1VC11\data\localweb\RobsonAndraDev\scss\index.scss" "D:\Program Files (x86)\EasyPHP-DevServer-14.1VC11\data\localweb\RobsonAndraDev\scss\index.css"
+pause
