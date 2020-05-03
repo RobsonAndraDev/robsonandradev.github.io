fe7380c643a59aff1494bfecbe73c95d2cc42749
RobsonAndradev <robsom.s.a@gmail.com>
2020-05-03 14:29:55 -0300

    Let code readable

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -41,43 +41,52 @@
 })(jQuery); // End of use strict
 
 function formatDatePt(date) {
-  var months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 
+  const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
     'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-  
-  return date.getDate() + ' de ' + months[date.getMonth()] + ' de ' +  date.getFullYear();
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  return `${day} de ${month} de ${year}`;
 }
 
-function getMediumStories() {
-  var content = $('#content');
-  var data = {
-    rss_url: 'https://medium.com/feed/@robsonandradev'
-  };
-  $.get('https://api.rss2json.com/v1/api.json', data, function (response) {
-    if (response.status == 'ok') {
-      console.log(response);
-      var output = '';
-      var label = '<i class="fa fa-tags"></i> ';
-      $.each(response.items, function (k, item) {
-        var description = /<(h4|blockquote)>(.*?)<\/(h4|blockquote)>/g.exec(item.description);
-        output += '<div class="post-preview">';
-        output += '<a href="' + item.link + '" target="_blank" >';
-        output += '<h2 class="post-title">';
-        output += item.title;
-        output += '</h2>';
-        output += '<h3 class="post-subtitle">';
-        output += description[2];
-        $.each(item.categories, function(l, category) {
-          if('robsonandradev' !== category) {
-            label += '<label class="post-meta art-tag">' + category + '</label> ';
-          }
-        });
-        output += '</h3></a>';
-        output += label;
-        output += '<br /><span class="post-meta">Publicado em ' + formatDatePt(new Date(item.pubDate)) + '</span>';
-        output += '</div>';
-        content.html( output );
-        label = '<i class="fa fa-tags"></i> ';
-      });
+const mountLabels = async (categories) => {
+  let label = '';
+  await categories.forEach(category => {
+    if('robsonandradev' !== category) {
+      label += `<label class="post-meta art-tag">${category}</label> `;
     }
   });
+  return label;
+};
+
+const mountPostsPage = (response) => {
+  const content = $('#content');
+  if (response.status == 'ok') {
+    // console.log(response);
+    let output = '';
+    let label = '<i class="fa fa-tags"></i> ';
+    const items = response.items;
+    items.forEach(async item => {
+      const description = /<(h4|blockquote)>(.*?)<\/(h4|blockquote)>/g.exec(item.description);
+      const subTitle = description[2];
+      const pubDate = formatDatePt(new Date(item.pubDate));
+      label += await mountLabels(item.categories);
+      output += '<div class="post-preview">';
+      output += `<a href="${item.link}" target="_blank" >`;
+      output += `<h2 class="post-title">${item.title}</h2>`;
+      output += `<h3 class="post-subtitle">${subTitle}</h3></a>`;
+      output += label;
+      output += `<br /><span class="post-meta">Publicado em ${pubDate}</span>`;
+      output += '</div>';
+      content.html( output );
+      label = '<i class="fa fa-tags"></i> ';
+    });
+  }
+};
+
+function getMediumStories() {
+  const mediumFeed = {
+    rss_url: 'https://medium.com/feed/@robsonandradev'
+  };
+  $.get('https://api.rss2json.com/v1/api.json', mediumFeed, mountPostsPage);
 }
